1c363a4d80283204bfa7b0f9958b3031b49a2a90
Michael Walker <michaelwwalker42@gmail.com>
2021-11-14 19:19:26 -0700

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#save-task");
 var tasksToDoEl =  document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {                    // function to dynamically create new element
+var createTaskHandler = function(event) {                    // function to dynamically create new element
+
+    event.preventDefault();                                // stops browser from reloading the page
+
     var listItemEl = document.createElement("li");    // create the element in the document
     listItemEl.className = "task-item";               // give element a class so CSS is applied
     listItemEl.textContent = "This is a new task.";    // create text for new element
     tasksToDoEl.appendChild(listItemEl);              // add new element to parent element
 };
 
-buttonEl.addEventListener("click", createTaskHandler);     // run createTaskHandler function when button is clicked
+formEl.addEventListener("submit", createTaskHandler);     // run createTaskHandler function when button is clicked
 
