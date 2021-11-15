158921a9c57353d6a8d8a2c8ddb23369b6547417
Michael Walker <michaelwwalker42@gmail.com>
2021-11-14 21:02:27 -0700

    refactor the code to organize functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl =  document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {                    // function to dynamically create new element
+//-------------------------------------taskFormHandler Function------------------------------------------------
+var taskFormHandler = function(event) {                    // function to dynamically create new element
     event.preventDefault();                                // stops browser from reloading the page
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    var taskDataObj = {               // package up data as an object
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(taskDataObj);      // send it as an argument to createTaskEl
+}
+//------------------------------------createTaskEl Function-----------------------------------------------------
+
+var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");  //   create list item
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");   // create div to hold task info and add to list item
     taskInfoEl.className = "task-info";             // give it a class name
 
-
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);      // add HTML content to div
 
     tasksToDoEl.appendChild(listItemEl);   // add entire list item to list
 };
+//-------------------------------------------------------------------------------------------------------------
 
-formEl.addEventListener("submit", createTaskHandler);     // run createTaskHandler function when button is clicked,
+formEl.addEventListener("submit", taskFormHandler);     // run taskFormHandler function when button is clicked,
                                                             // or 'enter' is pressed on keyboard                                        
