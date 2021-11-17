98f96bee435bf26a09a13d7b0ad17d2769b1fb03
Michael Walker <michaelwwalker42@gmail.com>
2021-11-16 18:11:24 -0700

    added some comments and fixed some errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,15 @@ var taskFormHandler = function(event) {                    // function to dynami
     var taskNameInput = document.querySelector("input[name='task-name']").value;  
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    if(!taskNameInput || !taskTypeInput) {                   // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {                   // check if input values are empty strings
         alert("You need to fill out the task form!");
         return false;
     }
-    formEl.reset();                  // reset method works specifically on form elements only
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
 
+  // check if task is new or one being edited by seeing if it has a data-task-id attribute
     var isEdit = formEl.hasAttribute("data-task-id");
     
     // has data attribute, so get task id and call function to complete edit process
@@ -54,6 +57,7 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);      // add HTML content to div
 
+    // create task actions (buttons and select) for task
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);             
     tasksToDoEl.appendChild(listItemEl);   // add entire list item to list
@@ -82,6 +86,7 @@ var createTaskActions = function(taskId) {
     deleteButtonEl.setAttribute("data-task-id", taskId);       // give button a custom data attribute
     actionContainerEl.appendChild(deleteButtonEl);           // attach button to actionContainerEl
 
+       // create change status dropdown
     var statusSelectEl = document.createElement("select");     // create select element
     statusSelectEl.className = "select-status";               // give element a class
     statusSelectEl.setAttribute("name", "status-change");      
@@ -94,8 +99,7 @@ var createTaskActions = function(taskId) {
        // for loop
     for (var i = 0; i < statusChoices.length; i++) {
         // create option element
-      var statusOptionEl = document.createElement("option");    
-      statusOptionEl = document.createElement("option");      
+      var statusOptionEl = document.createElement("option"); 
       statusOptionEl.textContent = statusChoices[i];         // statusChoices[i] returns value of the array at the given index
       statusOptionEl.setAttribute("value", statusChoices[i]);        
 
